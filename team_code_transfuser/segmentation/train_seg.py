-confusion matrix calculation corrected -> mIoU calculation is also corrected -confusion matrix is added to tensorboard

team_code_transfuser/segmentation/train_seg.py
@@ -7,12 +7,12 @@ from torch.utils.tensorboard import SummaryWriter
 import tqdm
 
 # from models.ERFNet.model import SemanticSegmentation as SegmentationModel
-from models.PIDNetLidar.model import PIDNet as SegmentationModel
+# from models.PIDNetLidar.model import PIDNet as SegmentationModel
 # from models.PIDNet.model import PIDNet as SegmentationModel
-# from models.TransfuserModel.model import SegmentationModel
+from models.TransfuserModel.model import SegmentationModel
 
 from seg_dataset import SegmentationDataset
-from utils import visualize_semantic_processed, smooth_loss, BondaryLoss, adjust_learning_rate, get_confusion_matrix
+from utils import visualize_semantic_processed, smooth_loss, BondaryLoss, adjust_learning_rate, get_confusion_matrix, visualize_cm, calculate_IoU_from_cm
 import config
 
 import os
@@ -21,12 +21,6 @@ import numpy as np
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 torch.cuda.empty_cache()
-
-train_it = 0
-val_it = 0
-total_iterations = 0
-# tensorboard writer
-writer = SummaryWriter()
 
 sem_loss = nn.CrossEntropyLoss(weight=config.class_weights)
 bd_loss = BondaryLoss()
@@ -48,10 +42,9 @@ def val_batch(batch, model : SegmentationModel):
     rgb, sem, pred_sem, loss = SegmentationModel.pass_from_model(model, batch, sem_loss, smooth_loss, bd_loss, device)
     sem = sem.cpu().detach().numpy()
     pred_sem = pred_sem.cpu().detach().numpy().argmax(1)
-    confusion_mat = get_confusion_matrix(sem, pred_sem, config.labels)
+    confusion_mat = get_confusion_matrix(sem, pred_sem)
 
     writer.add_scalar("Loss/val-per-batch", loss, val_it)
-    # TODO: add confusion matrix to tensorboard
 
     if val_it % args.num_per_log == 0: 
         loss = float(loss) 
@@ -84,7 +77,7 @@ def train_model(model, dataloader, optim, epoch):
 def val_model(model, dataloader, epoch): 
     model.eval()
     total_loss = 0
-    confusion_matrix = np.zeros((len(config.labels), len(config.labels)))
+    confusion_matrix = np.zeros((len(config.labels) + 1, len(config.labels) + 1))
 
     global val_it
     with torch.no_grad():    
@@ -95,12 +88,9 @@ def val_model(model, dataloader, epoch):
             val_it += 1
     avg_loss = total_loss / len(dataloader)    
 
-    pos = confusion_matrix.sum(1)
-    res = confusion_matrix.sum(0)
-    tp = np.diag(confusion_matrix)
-    IoU_array = (tp / np.maximum(1.0, pos + res - tp))
-    mean_IoU = IoU_array.mean()
+    mean_IoU = calculate_IoU_from_cm(confusion_matrix)
     writer.add_scalar("Avg-loss/val-per-epoch", avg_loss, epoch)
+    writer.add_figure("Confusion matrix", visualize_cm(confusion_matrix, config.labels), epoch)
     writer.add_scalar("Mean IoU/val-per-epoch", mean_IoU, epoch)
     return avg_loss, mean_IoU
 
@@ -191,4 +181,11 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    # Global variables
+    train_it = 0
+    val_it = 0
+    total_iterations = 0
+    # tensorboard writer
+    writer = SummaryWriter(f"runs/{args.model_name}")
+    
     main(args)

team_code_transfuser/segmentation/utils.py
@@ -7,6 +7,8 @@ import cv2
 import math
 from copy import deepcopy
 from sklearn.metrics import confusion_matrix
+import seaborn as sn
+import pandas as pd
 
 # from config import labels, SEM_COLORS
 import config
@@ -118,36 +120,47 @@ def adjust_learning_rate(optimizer, base_lr, max_iters, cur_iters, power=0.9, nb
         optimizer.param_groups[1]['lr'] = lr * nbb_mult
     return lr
 
-def get_confusion_matrix(label, pred, labels):
+def get_confusion_matrix(label, pred):
     """
         Calcute the confusion matrix by given label and pred
     """
     flat_label = label.flatten()
     flat_pred = pred.flatten()
 
-    cm = confusion_matrix(flat_label, flat_pred, labels)
+    cm = confusion_matrix(flat_label, flat_pred)
     return cm
 
-    # output = pred.cpu().numpy().transpose(0, 2, 3, 1)
-    # seg_pred = np.asarray(np.argmax(output, axis=3), dtype=np.uint8)
-    # seg_gt = np.asarray(
-    # label.cpu().numpy()[:, :size[-2], :size[-1]], dtype=np.int)
+def calculate_IoU_from_cm(confusion_mat): 
+    pos = confusion_mat.sum(1)
+    res = confusion_mat.sum(0)
+    tp = np.diag(confusion_mat)
+    IoU_array = (tp / np.maximum(1.0, pos + res - tp))
+    mean_IoU = IoU_array.mean()
+    return mean_IoU
 
-    # ignore_index = seg_gt != ignore
-    # seg_gt = seg_gt[ignore_index]
-    # seg_pred = seg_pred[ignore_index]
 
-    # index = (seg_gt * num_class + seg_pred).astype('int32')
-    # label_count = np.bincount(index)
-    # confusion_matrix = np.zeros((num_class, num_class))
-
-    # for i_label in range(num_class):
-    #     for i_pred in range(num_class):
-    #         cur_index = i_label * num_class + i_pred
-    #         if cur_index < len(label_count):
-    #             confusion_matrix[i_label, i_pred] = label_count[cur_index]
-    
-    return confusion_matrix
+def visualize_cm(confusion_mat, labels): 
+    class_names = {
+        1 : "building",
+        4 : "pedestrian",
+        5 : "pole",
+        6 : "line",
+        7 : "road",
+        8 : "sidewalk",
+        9 : "vegetation",
+        10: "vehicles",
+        18 : "traffic-light"
+    }
+    classes = ["empty"]
+    for label in labels: 
+        if label in class_names: 
+            classes.append(class_names[label])
+        else: 
+            classes.append("unknown class")
+    df_cm = pd.DataFrame(confusion_mat/np.sum(confusion_mat) * 10, index=[i for i in classes],
+                         columns=[i for i in classes])
+    plt.figure(figsize=(12, 7))    
+    return sn.heatmap(df_cm, annot=True).get_figure()
 
 def lidar_to_histogram_features(lidar):
     """